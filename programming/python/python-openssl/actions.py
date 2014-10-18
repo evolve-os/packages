12e4d2189d421871dd51d092e5959d5253ae2833
Justin Zobel <justin.zobel@gmail.com>
2014-10-18 15:23:06 +1030

    [ADD] python-openssl: Add to repos

---
+++ programming/python/python-openssl/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+# Created For Evolve OS
+
+from pisi.actionsapi import pythonmodules, pisitools
+
+
+def build():
+    pythonmodules.compile()
+
+
+#def check():
+#    pythonmodules.compile("test")
+
+
+def install():
+    pythonmodules.install()
+
+    pisitools.dodoc("LICENSE", "ChangeLog")
+
