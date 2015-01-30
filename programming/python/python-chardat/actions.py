a0e5c78feaab3d3793a90fadd2d07daac70ec339
Devon Bundy <devonbundy@gmail.com>
2015-01-29 21:09:11 -0500

    Added python-xlib, pyinotify and python-chardat

---
+++ programming/python/python-chardat/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+# Created For Evolve OS
+
+from pisi.actionsapi import pythonmodules, pisitools
+
+
+def build():
+    pythonmodules.compile()
+
+
+def check():
+    pythonmodules.compile("test")
+
+
+def install():
+    pythonmodules.install()
+
+    pisitools.dodoc("LICENSE")
